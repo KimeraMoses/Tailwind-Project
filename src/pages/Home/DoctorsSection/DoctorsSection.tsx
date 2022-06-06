181a302198610e724edc1b349a124ea3c5b05001
Moxhus <kimeramoses001@gmail.com>
2022-06-06 12:31:59 +0300

    finalizing deployable branch

--- src/pages/Home/DoctorsSection/DoctorsSection.tsx
+++ src/pages/Home/DoctorsSection/DoctorsSection.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import doctorsImage from "../../../assets/doctor-01.png";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Navigation } from "swiper";
 import DoctorCard from "src/components/DoctorCard/DoctorCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 // import { useSearchDoctors } from "@hooks";
@@ -39,20 +39,36 @@ export const DoctorsList = [
     rating: 2.5,
   },
   {
-    name: "Samuel Mutumba",
-    speciality: "Physiotherapist",
-    city: "Nailobi",
-    country: "Kenya",
-    image: doctorsImage,
-    rating: 5,
-  },
-  {
     name: "Nantale Becky",
     speciality: "Orthopaedic Specialist ",
     city: "Dodoma",
     country: "Tanzania",
     image: doctorsImage,
     rating: 3.5,
+  },
+  {
+    name: "Omoding Ronald",
+    speciality: "Physiotherapist",
+    city: "Amuria",
+    country: "Uganda",
+    image: doctorsImage,
+    rating: 5.0,
+  },
+  {
+    name: "Efena Efetie",
+    speciality: "Fertility Specialist",
+    city: "Abuja",
+    country: "Nigeria",
+    image: doctorsImage,
+    rating: 4.5,
+  },
+  {
+    name: "Samuel Mutumba",
+    speciality: "Physiotherapist",
+    city: "Nailobi",
+    country: "Kenya",
+    image: doctorsImage,
+    rating: 5,
   },
 ];
 
@@ -63,17 +79,17 @@ const DoctorsSection: React.FunctionComponent = () => {
   return (
     <div className="w-full my-6 mb-10">
       <h2 className="text-4xl text-accent capitalize text-center mb-3 font-semibold font-Poppins select-none">
-        Book Our Best Doctors
+        Book Our Best Specialists
       </h2>
 
       <div className="flex justify-center  my-5 select-none mx-32">
         <Swiper
           navigation={true}
           modules={[Navigation]}
-          spaceBetween={30}
+          spaceBetween={20}
           slidesPerView={3}
           className="w-auto"
-          style={{ padding: "0 30px" }}
+          style={{ padding: "0 20px" }}
         >
           {DoctorsList.map((doctor) => {
             return (
